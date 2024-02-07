5b50135044815ace016688df778f91e5d5213f75
Joshua Schmid <jaiks@posteo.de>
2024-02-07 11:14:22 +0100

    poc
    Signed-off-by: Joshua Schmid <jaiks@posteo.de>

--- kong/db/strategies/off/consumers.lua
+++ kong/db/strategies/off/consumers.lua
@@ -10,46 +10,30 @@ local Consumers = {}
 
 function Consumers:select(id)
   -- Retrieve a consumer by id from the admin api :8001/consumers/:id
+  local c = http.new()
 
-  local correlation_id, err = dp.get_consumer({id = id})
+  id = id.id or id
+  if not id then
+    return nil, "id is required"
+  end
+
+  local url = "http://localhost:8001/consumers/" .. id
+
+  local response, err = c:request_uri(url, {
+    method = "GET",
+    headers = {
+      ["Content-Type"] = "application/json",
+    },
+  })
   if err then
     return nil, err
   end
 
-  -- ensure that this isn't blocking
-  local results = dp.wait_for_results(correlation_id)
-  print("results = " .. require("inspect")(results))
+  local res = cjson.decode(response.body)
+  print("XXX: FETCHING CONSUMER FROM CP")
+  print("res = " .. require("inspect")(res))
 
-  return results
-
+  return res
 end
-
--- function Consumers:select(id)
---   -- Retrieve a consumer by id from the admin api :8001/consumers/:id
---   local c = http.new()
-
---   id = id.id or id
---   if not id then
---     return nil, "id is required"
---   end
-
---   local url = "http://localhost:8001/consumers/" .. id
-
---   local response, err = c:request_uri(url, {
---     method = "GET",
---     headers = {
---       ["Content-Type"] = "application/json",
---     },
---   })
---   if err then
---     return nil, err
---   end
-
---   local res = cjson.decode(response.body)
---   print("XXX: FETCHING CONSUMER FROM CP")
---   print("res = " .. require("inspect")(res))
-
---   return res
--- end
 
 return Consumers
