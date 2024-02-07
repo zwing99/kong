c09f724a8be3c2e9dd5d16b95a71b56eefd0ff8f
Joshua Schmid <jaiks@posteo.de>
2024-02-07 14:42:58 +0100

    poc
    Signed-off-by: Joshua Schmid <jaiks@posteo.de>

--- kong/db/strategies/off/consumers_lazy.lua
+++ kong/db/strategies/off/consumers_lazy.lua
@@ -1,38 +1,21 @@
 local kong      = kong
-local fmt       = string.format
-local http      = require "resty.http"
-local cjson    = require "cjson.safe"
+local fetch_from_cp = require "kong.db.utils".fetch_from_cp
+
 
 local Consumers = {}
 
-
 function Consumers:select(id)
   -- Retrieve a consumer by id from the admin api :8001/consumers/:id
-  local c = http.new()
-
   id = id.id or id
   if not id then
     return nil, "id is required"
   end
+  return fetch_from_cp("/consumers/" .. id)
+end
 
-  -- FIXME: include workspaces to make this work correctly in EE
-  local url = "http://localhost:8001/consumers/" .. id
-
-  local response, err = c:request_uri(url, {
-    method = "GET",
-    headers = {
-      ["Content-Type"] = "application/json",
-    },
-  })
-  if err then
-    return nil, err
-  end
-
-  local res = cjson.decode(response.body)
-  print("XXX: FETCHING CONSUMER FROM CP")
-  print("res = " .. require("inspect")(res))
-
-  return res
+function Consumers:select_by_username(username)
+  -- Retrieve a consumer by username from the admin api :8001/consumers/:username
+  return fetch_from_cp("/consumers/" .. username)
 end
 
 return Consumers
--- kong/db/strategies/off/keyauth_credentials_lazy.lua
+++ kong/db/strategies/off/keyauth_credentials_lazy.lua
@@ -1,38 +1,20 @@
 local kong      = kong
-local fmt       = string.format
-local http      = require "resty.http"
-local cjson    = require "cjson.safe"
+local fetch_from_cp = require "kong.db.utils".fetch_from_cp
 
 local KeyauthCredentials = {}
 
 function KeyauthCredentials:select_by_key(key)
+  local cred, err = fetch_from_cp("/key-auths/" .. key)
 
-  local c = http.new()
-
-  local url = "http://localhost:8001/key-auths/" .. key
-
-  local response, err = c:request_uri(url, {
-    method = "GET",
-    headers = {
-      ["Content-Type"] = "application/json",
-    },
-  })
-  if err then
+  if not cred then
     return nil, err
   end
-
-  local cred = cjson.decode(response.body)
 
   if cred.ttl == 0 then
     kong.log.debug("key expired")
 
     return nil
   end
-  print("XXX: FETCHING KEYAUTH CREDENTIAL FROM CP")
-  if cred.message == "Not found" then
-    return nil, nil -- -1
-  end
-
   return cred, nil, cred.ttl
 end
 
